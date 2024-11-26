4cce58eafb03cdff2dfe330e41d56d55172a6703
BrynDeibel <mgd004@lvc.edu>
2024-11-26 16:18:48 -0500

    Remove test file

--- ImplTest.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import sys
-sys.path.append('..')
-from DB_Impls.assignment_fb_db_impl import *
-
-impl = AssignmentInterfaceFirebase()
-read = asyncio.run(impl.readAssignments())
-
-print ("hellow")
